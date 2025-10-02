b13a0b93ef90ad027dedb54c92a639c05a68bc81
Apex-dev01 <recovery12ws@outlook.com>
2025-10-02 10:26:57 -0500

    Create UIController.js

---
+++ src/ui/UIController.js
@@ -0,0 +1,15 @@
+// UIController.js - Manages user interface interactions
+
+export class UIController {
+  constructor(audioEngine, patternManager, transportController) {
+    this.audioEngine = audioEngine;
+    this.patternManager = patternManager;
+    this.transportController = transportController;
+  }
+
+  refreshPatternList() {
+    // Stub method for refreshing pattern list
+  }
+}
+
+export default UIController;
